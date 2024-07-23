0ee56cc1a9e8a829baa52fef770210d4fd098cdb
mingdodev <mingdo.dev@gmail.com>
2024-07-23 13:58:48 +0900

    PaymentService 개발 2

--- src/main/java/tobyspring/hellospring/Payment.java
+++ src/main/java/tobyspring/hellospring/Payment.java
@@ -1,4 +1,68 @@
 package tobyspring.hellospring;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Payment {
+    // id는 주로 Long
+    // 타입은 보통 private로 지정
+    // 다른 클래스에서 필드에 접근해 수정하지 못하도록
+    // (지금은 뭐 상관은 없음)
+    // BigDeciaml 돈을 다룰 때는! 더블은 오차가 발생하므로
+
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime validUntil;
+
+    // 생성과 동시에 값을 집어넣음 -> 생성자
+    // 점차 값이 생김 -> setter
+
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", validUntil=" + validUntil +
+                '}';
+    }
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
 }

f53f8022af291f3fac0e8b49ab1a611a84fb02d1
mingdodev <mingdo.dev@gmail.com>
2024-07-23 14:29:25 +0900

    PaymentService 개발 3

---
+++ src/main/java/tobyspring/hellospring/ExRateData.java
@@ -0,0 +1,15 @@
+package tobyspring.hellospring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+    // 데이터 전달을 위한 데이터 타입
+    // DTO에서 생성자를 만들고 getter 만들었던 것처럼
+    // 저렇게 생성자를 만든다 특이하죠?
+    // 한번 값을 저장하면 수정 불가, 그를 계속 이용
+    // @JsonIgnoreProperties(ignoreUnknown = true) 매핑하지 않는 변수는 무시해준다!
+}

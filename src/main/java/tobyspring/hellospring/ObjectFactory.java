480c798ac9b9ddb28f80b7789fd602857b541f72
mingdodev <mingdo.dev@gmail.com>
2024-07-25 16:05:53 +0900

    구성정보를 가져오는 다른 방법

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,19 +1,24 @@
 package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@ComponentScan
 public class ObjectFactory {
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
-    }
+    // 이게 어떻게 가능해?
+    // 의존 관계를 어떻게 알아서 찾는 거야?
+    // 스프링이 필요한 것은 빈에 관련된 정보
+    // 컴포넌트를 지정하고 그 컴포넌트들을 설정해서 정보를 찾아봐
 
-    // 클래스의 오브젝트를 만듦 = 하나의 빈을 만듦
+    // Error creating bean with name 'paymentService' defined in file
+    // 아!~! 맞다맞다
+    // 컴포넌트 애노테이션을 여러 개에 붙이면 에러가 난다.
+    // getBean(PaymentService.class)에서 해당 타입의 빈을 여러 개 찾으면
+    // 의존관계 설정에 혼란을 겪겠지
+    // 전에 그랬었다 이해 완료 ~!~!!!~!~!~!
 
-    // 이렇게 분리하면 메소드 이름이 분명하다. 변경이 필요할 때 찾아가기 편해짐.
-    public ExRateProvider exRateProvider() {
-        return new SimpleExRateProvider();
-    }
+    // 컴포넌트 스캔을 많이 사용하지만, 빈 팩토리에 직접 구성 정보를 전달하는 경우도 있다.
 }

676f71e86c2415a1e1836af9770b2c020f07efa8
mingdodev <mingdo.dev@gmail.com>
2024-07-25 14:06:15 +0900

    관계설정 책임의 분리

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -7,9 +7,9 @@ import java.time.LocalDateTime;
 public class PaymentService {
     private final ExRateProvider exRateProvider;
 
-    public PaymentService() {
+    public PaymentService(ExRateProvider exRateProvider) {
         // 인터페이스 타입의 변수에 구현체를 담음. 이부분만 변경하면 됨
-        this.exRateProvider = new SimpleExRateProvider();
+        this.exRateProvider = exRateProvider;
     }
 
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {

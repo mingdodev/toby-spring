7d9461e4b5bb42380d7b38ce6a093e27d98f8812
mingdodev <mingdo.dev@gmail.com>
2024-07-25 15:58:33 +0900

    스프링 컨테이너와 의존관계 주입

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,12 +1,19 @@
 package tobyspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    // 클래스의 오브젝트를 만듦 = 하나의 빈을 만듦
+
     // 이렇게 분리하면 메소드 이름이 분명하다. 변경이 필요할 때 찾아가기 편해짐.
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
+        return new SimpleExRateProvider();
     }
 }

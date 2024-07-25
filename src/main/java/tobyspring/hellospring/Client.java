579afada7f8b806b92557492dfefdfb7e8046d7f
mingdodev <mingdo.dev@gmail.com>
2024-07-25 17:33:03 +0900

    의존성 역전 원칙 (DIP)

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -2,26 +2,18 @@ package tobyspring.hellospring;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import tobyspring.hellospring.payment.Payment;
+import tobyspring.hellospring.payment.PaymentService;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.util.concurrent.TimeUnit;
 
 public class Client {
     public static void main(String[] args) throws IOException, InterruptedException {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
-        Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment1);
-        System.out.println("--------------");
-
-        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment2);
-        System.out.println("--------------");
-
-        TimeUnit.SECONDS.sleep(3);
-        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment3);
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
     }
 }

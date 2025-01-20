541d14bfc105e018f468bbfb6ac7f7f49e3f8f9d
mingdodev <mingdo.dev@gmail.com>
2025-01-20 23:26:24 +0900

    스프링이 제공하는 템플릿

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -5,7 +5,9 @@ import api.ErApiExRateExtractor;
 import api.SimpleApiExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import tobyspring.hellospring.exrate.CachedExRateProvider;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
 import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.exrate.WebApiExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
@@ -18,13 +20,10 @@ public class PaymentConfig {
     public PaymentService paymentService() { return new PaymentService(exRateProvider(), clock()); };
 
     @Bean
-    public ExRateProvider cachedExRateProvider() { return new CachedExRateProvider(exRateProvider()); }
+    public RestTemplate restTemplate() { return new RestTemplate(); }
 
     @Bean
-    public ApiTemplate apiTemplate() { return new ApiTemplate(new SimpleApiExecutor(), new ErApiExRateExtractor()); }
-
-    @Bean
-    public ExRateProvider exRateProvider() { return new WebApiExRateProvider(apiTemplate()); }
+    public ExRateProvider exRateProvider() { return new RestTemplateExRateProvider(restTemplate()); }
 
     @Bean
     public Clock clock() { return Clock.systemDefaultZone(); }
